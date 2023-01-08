d57611bcc6cf07c8612e08788aa727c8e8b99afc
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-08 01:59:08 -0600

    Add textModels dict to __init__.py
    Add textModels dictionary to __init__.py. Move the definition from
    app.py to __init__.py to simplify app.py.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,29 @@
 from os import environ
 import openai
 
+# constant values
+TEXT_KEY = "text"
+EDIT_KEY = "edit"
+ID_KEY = "id"
+DATA_KEY = "data"
+
 # get API key from usr folder
 openai.api_key = environ["OPENAI_API_KEY"]
+
+# list of text models
+textModels = dict()
+
+textModels[TEXT_KEY] = []
+textModels[EDIT_KEY] = []
+
+# print the names of the models that you can use
+for model in openai.Model.list()[DATA_KEY]:
+    if model[ID_KEY].count(TEXT_KEY) > 0:
+        if model[ID_KEY].count(EDIT_KEY) > 0:
+            textModels[EDIT_KEY].append(model[ID_KEY])
+            pass
+        else:
+            textModels[TEXT_KEY].append(model[ID_KEY])
+            pass
+        pass
+    pass
