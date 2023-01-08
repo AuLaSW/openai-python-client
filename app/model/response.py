74dddba23f746244128c61c0f5c6f5850d7da141
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-08 16:18:23 -0600

    Create response.py
    Create Response class.
    
    This class is the response that we get back from the OpenAI API. Still a
    bit of work to do, but it works as-is. Just have to make it work
    together better.

---
+++ app/model/response.py
@@ -0,0 +1,38 @@
+"""
+Class Response:
+
+A class that turns the response JSON from the OpenAI API
+into an object that can be used within Python
+"""
+
+
+class Response:
+    def __init__(self):
+        self.obj = ""
+        self.model = ""
+        self.text = ""
+        self.finish_reason = ""
+    pass
+
+    def parseResponse(self, response):
+        self.obj = response["object"]
+        self.text = response["choices"][0]["text"]
+        self.index = response["choices"][0]["index"]
+
+        if "text_completion" in self.obj:
+            self.model = response["model"]
+            self.finish_reason = response["choices"][0]["finish_reason"]
+
+
+if __name__ == "__main__":
+    from request.completion import CompletionRequest
+
+    req = CompletionRequest()
+    req.requestDict[req.OPTIONAL]["prompt"] = "tell me a joke"
+
+    response = Response()
+
+    response.parseResponse(req.getResponse(req.requestDict))
+
+    print(response.text, response.index)
+    pass
