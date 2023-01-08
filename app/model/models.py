1ab61ecbf938e1cc97f0d3a998b59cbbfbaf6853
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-08 14:58:19 -0600

    Create Models class
    Create Models class in models.py.
    
    The class makes it much easier to work with the models list and
    generates a prettier models list for people to read.

--- app/model/models.py
+++ app/model/models.py
@@ -1,36 +1,74 @@
 import openai
 
 
-# sort the textModels into a more
-# friendly list.
-def sortModels(modelList):
-    return modelList
+class Models:
+    def __init__(self):
+        self.COMPLETION = "Completion"
+        self.EDIT = "Edit"
+        # dictionary of models
+        self.models = {
+            self.COMPLETION: [],
+            self.EDIT: []
+        }
 
+        # populate the model dictionary
+        self.generateModels()
 
-# retreives the different GPT models
-# from OpenAI and creates a list with
-# all of the valid IDs
-def getModels():
-    # constant values
-    TEXT_KEY = "text"
-    ID_KEY = "id"
-    DATA_KEY = "data"
+    def __str__(self):
+        strModels = "Usable Models:\n"
 
-    # list of text models
-    models = []
+        strModels += "  " + self.COMPLETION + " Models:\n"
 
-    # print the names of the models that you can use
-    for model in openai.Model.list()[DATA_KEY]:
-        if model[ID_KEY].startswith(TEXT_KEY) > 0:
-            models.append(model["id"])
-            pass
-        pass
-    return sortModels(models)
+        for key in self.models[self.COMPLETION]:
+            strModels += "    " + key + "\n"
 
+        strModels += "  " + self.EDIT + " Models:\n"
 
-# generate list of text models
-textModels = getModels()
+        for key in self.models[self.EDIT]:
+            strModels += "    " + key + "\n"
+
+        return strModels
+
+    def __contains__(self, item):
+        return item in self.models
+
+    # returns a dictionary of usable text models
+    def generateModels(self):
+        # loop through the list of models
+        for model in openai.Model.list()["data"]:
+            # only focus on the id of the model
+            modelID = model["id"]
+
+            # is the model a text model
+            textModel = "text" in modelID
+
+            # is the model not a search or code model?
+            # I'm not sure what the : is used for in the model
+            # names, but I don't want to use those
+            codeModel = "code" in modelID
+            searchModel = "search" in modelID
+            simModel = "similarity" in modelID
+            insModel = "insert" in modelID
+            embModel = "embedding" in modelID
+            colModel = ":" in modelID
+            validModel = (not codeModel and not searchModel
+                          and not simModel and not colModel
+                          and not insModel and not embModel)
+
+            # is the model an edit model?
+            editModel = "edit" in modelID
+
+            if textModel and validModel:
+                if editModel:
+                    self.models[self.EDIT].append(modelID)
+                else:
+                    self.models[self.COMPLETION].append(modelID)
+
+    def getModels(self):
+        return self.models
+
 
 if __name__ == "__main__":
-    print(textModels)
-    pass
+    model = Models()
+
+    print(model)
