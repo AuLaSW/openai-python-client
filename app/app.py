60c1242bcc75ca9e7a2bf8a741c307599a5ca833
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-07 23:42:11 -0600

    Add to app.py
    Add some lines to app.py. Just working with everything to figure out how
    it works.

--- app/app.py
+++ app/app.py
@@ -1,1 +1,12 @@
+import openai
 
+# get API key from usr folder
+openai.api_key = "API_KEY"
+
+# get a dictionary of models
+models = openai.Model.list()
+
+# print the names of the models that you can use
+for model in models["data"]:
+    print(model["id"])
+    pass
