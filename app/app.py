8684a68a7afa8e1f9271f7a7648497d844b1c647
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-08 02:03:30 -0600

    Clear app.py
    Remove all lines from app.py.
    
    Everything was moved to __init__.py, so now the file is empty. Plenty of
    other things will be moving around, so it will fill up soon enough.

--- app/app.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-# get a dictionary of models
-models = openai.Model.list()
-
-# print the names of the models that you can use
-for model in models["data"]:
-    print(model["id"])
-    pass
