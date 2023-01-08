8fc4493c4983f5bf12efc21b1efe67fbf6c0f226
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-08 02:45:01 -0600

    Clean getModels()
    Clean getModels() function. Simplified the implementation a bit.

--- app/model/aiModels.py
+++ app/model/aiModels.py
@@ -4,25 +4,17 @@ import openai
 def getModels():
     # constant values
     TEXT_KEY = "text"
-    EDIT_KEY = "edit"
     ID_KEY = "id"
     DATA_KEY = "data"
 
     # list of text models
-    textModels = dict()
-
-    # set up keys for the text models
-    textModels[TEXT_KEY] = []
-    textModels[EDIT_KEY] = []
+    textModels = []
 
     # print the names of the models that you can use
     for model in openai.Model.list()[DATA_KEY]:
-        if model[ID_KEY].count(TEXT_KEY) > 0:
-            if model[ID_KEY].count(EDIT_KEY) > 0:
-                textModels[EDIT_KEY].append(model[ID_KEY])
-                pass
-            else:
-                textModels[TEXT_KEY].append(model[ID_KEY])
+        if model[ID_KEY].startswith(TEXT_KEY) > 0:
+            if len(model[ID_KEY].split("-")) == 3:
+                textModels.append(model["id"])
                 pass
             pass
         pass
