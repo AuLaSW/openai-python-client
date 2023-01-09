e887376a3615e859840cecdf08fffb5af55934a8
Austin <austin.swanlaw@gmail.com>
2023-01-09 13:13:55 -0600

    Create controller.py
    Create file for controller class.

---
+++ app/controller/controller.py
@@ -0,0 +1,20 @@
+from model.request.completion import CompletionRequest as CR
+from model.request.edit import EditRequest as ER
+from model.models import Models
+from model.response import Response
+from view.view import View
+import openai
+
+"""
+Class Controller:
+
+This class manages the data model and the view model.
+"""
+
+
+class Controller:
+    pass
+
+
+if __name__ == "__main__":
+    pass
