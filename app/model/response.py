c50a7e9655b0ea5be0656f72c96109555efc84f3
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-08 18:59:59 -0600

    Remove blocked parseResponse() in Response class
    Remove the parseResponse() function that was removed last commit.

--- app/model/response.py
+++ app/model/response.py
@@ -16,17 +16,6 @@ class Response:
             self.model = response["model"]
             self.finish_reason = response["choices"][0]["finish_reason"]
 
-    """
-    def parseResponse(self, response):
-        self.obj = response["object"]
-        self.text = response["choices"][0]["text"]
-        self.index = response["choices"][0]["index"]
-
-        if "text_completion" in self.obj:
-            self.model = response["model"]
-            self.finish_reason = response["choices"][0]["finish_reason"]
-    """
-
 
 if __name__ == "__main__":
     pass
