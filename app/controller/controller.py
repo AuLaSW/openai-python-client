de2650efdc2f64932c1a79e93dd450fbf15c2fd2
Austin <austin.swanlaw@gmail.com>
2023-01-09 14:57:44 -0600

    Add functions to Controller class
    Add some functions to the Controller class. I am still figuring out how
    to best manage the MVC pattern I am trying to create, but so far this
    seems to be a good starting point.

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -1,9 +1,8 @@
-from model.request.completion import CompletionRequest as CR
-from model.request.edit import EditRequest as ER
+from model.request.completion import CompletionRequest
+from model.request.edit import EditRequest
 from model.models import Models
 from model.response import Response
 from view.view import View
-import openai
 
 """
 Class Controller:
@@ -13,8 +12,63 @@ This class manages the data model and the view model.
 
 
 class Controller:
-    pass
+    def __init__(self):
+        # initialized variables
+        
+        # different models we can use
+        self.models = Models()
+        # the view
+        self.view = View()
+        
+        # uninitialized variables
+        
+        # the request we are making
+        self.request = None
+        # the response
+        self.response = None
+    
+    # creates a request
+    def buildRequest(self, req):
+        self.request = self.view.getRequest(req)
+        self.response = self.request.getResponse()
+        
+        self.view.updateResponse(self.response)
+    
+    # build an edit request
+    def buildEditRequest(self):
+        if not isinstance(self.request, EditRequest):
+            self.request = EditRequest()
+        self.buildRequest(self.request)
+        
+    # build a completion request
+    def buildCompletionRequest(self):
+        if not isinstance(self.request, CompletionRequest):
+            self.request = CompletionRequest()
+        self.buildRequest(self.request)
+    
+    # reset the current request
+    def resetRequest(self):
+        if isinstance(self.request, CompletionRequest):
+            self.request = CompletionRequest()
+        else:
+            self.request = EditRequest()
+            
+    # initialize view
+    def initView(self):
+        return self.view.init()
+    
+    # get the next view
+    def getView(self, nextView):
+        return self.view.get(nextView)
 
 
 if __name__ == "__main__":
+    
+    controller = Controller()
+    
+    nextView = controller.initView()
+    
+    for:
+        nextView = controller.getView(nextView)
+    
     pass
