37e23d1eac8c8c4e5d10bbf55b3e80ecc9db48db
Austin <austin.swanlaw@gmail.com>
2023-01-10 09:31:07 -0600

    Create view.py
    This class will handle the view operations with the controller.

---
+++ app/view/view.py
@@ -0,0 +1,23 @@
+# view.py
+"""
+Class View:
+
+Manages the GUI, interacts with the Controller and Model classes.
+"""
+
+
+class View:
+    def __init__(self):
+        pass
+    
+    def get(self, nextView):
+        pass
+    
+    def initView(self):
+        pass
+        
+    def getRequest(self, req):
+        pass
+
+if __name__ == "__main__":
+    pass
