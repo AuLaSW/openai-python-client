0a56ab1a8fc43bd19c7a8b678d6623ca873e38e3
Austin <austin.swanlaw@gmail.com>
2023-01-10 15:57:39 -0600

    Create startwindow.py
    This is a test run on a window extending the Window class. This would
    theoretically be the first window that appears when the application is
    started.

---
+++ app/view/startwindow.py
@@ -0,0 +1,47 @@
+# startwindow.py
+from app.view.window import Window
+import tkinter as tk
+
+
+class StartWindow(Window):
+    def __init__(self, controller):
+        super.__init__()
+        
+        self.frameMain = tk.Frame(master=self.window)
+        
+        self.buttonCompletionForm = tk.Button(
+            master=self.frameMain
+            text="Completion Form"
+            command=getCompletionForm
+        )
+        self.buttonEditForm = tk.Button(
+            master=self.frameMain
+            text="Edit Form"
+            command=getEditForm
+        )
+        
+        self.buttonCompletionForm.grid(
+            column=0,
+            row=0,
+            padx = 10,
+            pady = 10
+        )
+        
+        self.buttonCompletionForm.grid(
+            column=0,
+            row=0,
+            padx = 10,
+            pady = 10
+        )
+        
+        self.frameMain.pack()
+        
+    def getCompletionForm(self, controller):
+        self.controller.getCompletionForm()
+    
+    def getEditForm(self, controller):
+        self.controller.getEditForm()
+
+
+if __name__ == "__main__":
+    pass
