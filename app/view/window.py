261e9f473c49e819fb493e9cdf1104ce123a8de1
Austin <austin.swanlaw@gmail.com>
2023-01-10 15:56:13 -0600

    Create window.py
    This module holds the abstract class Window upon which all of the
    windows the application will use will be based off of. This should make
    adding windows to the application easier.

---
+++ app/view/window.py
@@ -0,0 +1,41 @@
+# window.py
+
+"""
+Class Window:
+
+Creates the different windows used by the application and acts
+as a buffer between the View class and the tkinter package.
+
+This is an abstract class. All of the windows produced by
+the application should have their own class for extensibility
+reasons.
+"""
+
+
+class Window:
+    def __init__(self, controller):
+        self.window = tk.Tk()
+        self.controller = controller
+        
+        self.window.title("openai-python-client)
+    
+    def init(self):
+        self.window.mainloop()
+    
+    # draw the window
+    def draw(self):
+        return NotImplementedError()
+
+    # get return value from window
+    # if it has a return
+    # this should yield a dictionary?
+    # haven't decided yet
+    def get(self)
+        return NotImplementedError()
+        
+    def set(self, input)
+        return NotImplementedError()
+
+
+if __name__ == "__main__":
+    pass
