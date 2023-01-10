e3808ecdd7ff6aa58d516ed5fd000d6fbe811633
Austin <austin.swanlaw@gmail.com>
2023-01-10 09:45:54 -0600

    Add controller binding to view.py
    Bind the controller object to the view object.
    
    This allows the view to directly access the controller and its methods.

--- app/view/view.py
+++ app/view/view.py
@@ -7,8 +7,13 @@ Manages the GUI, interacts with the Controller and Model classes.
 
 
 class View:
-    def __init__(self):
-        pass
+    def __init__(self, controller):
+        # bind the controller to the view object
+        # by passing the controller to the view,
+        # we can bind controller functions to view
+        # functions and have a better time working
+        # with everything
+        self.controller = controller
     
     def get(self, nextView):
         pass
