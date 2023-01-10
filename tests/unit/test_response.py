5b31e4e8415c78572422baf62b2c9f96e824a36d
Austin <austin.swanlaw@gmail.com>
2023-01-10 11:53:14 -0600

    Add tests to test_response.py
    Add some simple tests to the TestResponse class to test the functions
    within the class.
    
    Setup a dictionary with an empty response from the OpenAI server as a
    mock server response. This gives more control over the content of the
    response and lets us better test against it.

--- tests/unit/test_response.py
+++ tests/unit/test_response.py
@@ -1,1 +1,35 @@
 # test_response.py
+import unittest
+
+from app.model.response import Response
+
+
+class TestResponse(unittest.TestCase):
+    def setUp(self):
+        self.dictionary = {
+            "object": "",
+            "choices": [ {
+                "text": ""
+                "index": 0
+                "finish_reason": ""
+                }
+            ],
+            "model": ""
+        }
+
+        self.response = Response(self.dictionary)
+
+    def test_getText(self):
+        result = self.response.getText()
+        reference = self.dictionary["choices"][0]["text"]
+
+        self.assertEqual(result, reference)
+
+    def test_getFinishReason(self):
+        result = self.response.getFinishReason()
+        reference = self.dictionary["choices"][0]["finish_reason"]
+
+        self.assertEqual(result, reference)
+
+if __name__ == "__main__":
+    pass
