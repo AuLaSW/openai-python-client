c5d293c9bf1d51fbde818ce4e23818d7ad2254a7
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-10 20:05:17 -0600

    Modify startwindow to not take controller object
    Modify startwindow so that it does not take a controller object. Makes
    for easier testing. Correct import statements. Correct tk.Button inputs.
    
    Wanted to create a branch so this didn't get mixed into the main branch.
    Will likely rename this branch for GUI development.

--- app/view/startwindow.py
+++ app/view/startwindow.py
@@ -1,47 +1,51 @@
 # startwindow.py
-from app.view.window import Window
+from window import Window
 import tkinter as tk
 
 
 class StartWindow(Window):
-    def __init__(self, controller):
-        super.__init__()
-        
+    # def __init__(self, controller):
+    def __init__(self):
+        super().__init__(None)
+
         self.frameMain = tk.Frame(master=self.window)
-        
+
         self.buttonCompletionForm = tk.Button(
-            master=self.frameMain
-            text="Completion Form"
-            command=getCompletionForm
+            master=self.frameMain,
+            text="Completion Form",
+            command=self.getCompletionForm
         )
         self.buttonEditForm = tk.Button(
-            master=self.frameMain
-            text="Edit Form"
-            command=getEditForm
+            master=self.frameMain,
+            text="Edit Form",
+            command=self.getEditForm
         )
-        
+
         self.buttonCompletionForm.grid(
             column=0,
             row=0,
             padx=10,
             pady=10
         )
-        
-        self.buttonCompletionForm.grid(
-            column=0,
+
+        self.buttonEditForm.grid(
+            column=1,
             row=0,
             padx=10,
             pady=10
         )
-        
+
         self.frameMain.pack()
-        
-    def getCompletionForm(self, controller):
-        self.controller.getCompletionForm()
-    
-    def getEditForm(self, controller):
-        self.controller.getEditForm()
+
+    def getCompletionForm(self):
+        # self.controller.getCompletionForm()
+        pass
+
+    def getEditForm(self):
+        # self.controller.getEditForm()
+        pass
 
 
 if __name__ == "__main__":
-    pass
+    window = StartWindow()
+    window.init()
