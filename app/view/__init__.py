1d354ec5bc87f38208cbe87298a46d5f6b25c91d
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-10 20:21:23 -0600

    Complete __init__.py for the view package

---
+++ app/view/__init__.py
@@ -0,0 +1,9 @@
+# view package __init__
+
+# import all modules from package
+from . import startwindow
+from . import view
+from . import window
+
+# packages to import
+__all__ = ["startwindow", "view", "window"]
