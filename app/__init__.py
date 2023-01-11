ab51e59594085aa06bcbcb9f2a05358629a0be75
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-10 20:22:53 -0600

    Complete __init__.py for app package

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+# app package __init__
+
+# import all modules from package
+from . import controller
+from . import model
+from . import view
+
+# packages to import
+__all__ = ["controller", "model", "view"]
