076b565d92b2e5bff6076f57a11a28d8f5df6b61
Austin <austin.swanlaw@gmail.com>
2023-01-11 14:38:29 -0600

    Add comments to settingsframe.py
    Add comments for clarification of functions and class purpose to
    SettingsFrame class.

--- app/view/frame/settingsframe.py
+++ app/view/frame/settingsframe.py
@@ -2,6 +2,14 @@
 from baseframe import BaseFrame
 import tkinter as tk
 
+"""
+Class SettingsFrame:
+
+This class is for a frame that holds settings for
+a user to change. Created to work with the Completion
+and Edit requests to OpenAI.
+"""
+
 
 class SettingsFrame(BaseFrame):
     def __init__(self, main, controller):
@@ -11,6 +19,11 @@ class SettingsFrame(BaseFrame):
         
         self.row = 0
 
+    # creates all of the widgets for
+    # the frame. This default method
+    # can be overriden for different
+    # types of input in other settings
+    # classes.
     def create(self):
         for key, value in self.settings:
             switch type(value):
@@ -26,6 +39,9 @@ class SettingsFrame(BaseFrame):
                 default:
                     break
 
+    # default setting generator. Cleans
+    # up the code and makes it easier to
+    # define a new setting type
     def baseSetting(self, tkFunc, label, default, **kwargs):
         # generate label on the left
         tk.Label(
@@ -51,13 +67,17 @@ class SettingsFrame(BaseFrame):
             pady=10
         )
         
+        # move down one row
         self.row += 1
 
+    # string setting input
     def strSetting(self, label, default):
         self.baseSetting(tk.Entry, label, default, **kwargs)
 
+    # integer setting input
     def intSetting(self, label, default)
         self.baseSetting(tk.Entry, label, default, **kwargs)
 
+    # boolean setting input
     def boolSetting(self, label, default)
         self.baseSetting(tk.Checkbutton, label, default, **kwargs)
