2c571932a5e0562a7f3c69337e33754b0cced5bf
Austin <austin.swanlaw@gmail.com>
2023-01-12 11:48:13 -0600

    Remove getters and setters from CompletionRequest
    Removed all getters and setters except for getResponse() and
    addToPrompt().
    
    These functions are obsolete since Request has get() and set().
    getResponse() is required for the class to work and addToPrompt() allows
    the user to add a message to a previous prompt, which is differet from
    the regular get() and set(), though it could be replicated with them.

--- openai-client/model/request/completion.py
+++ openai-client/model/request/completion.py
@@ -59,82 +59,10 @@ class CompletionRequest(Request):
                 )
             )
 
-    def getModel(self):
-        return self.get(self.REQUIRED, "model")
-
-    def setModel(self, input):
-        self.set(self.REQUIRED, "model", input)
-
-    def getPrompt(self):
-        return self.get(self.OPTIONAL, "prompt")
-
-    def setPrompt(self, input):
-        self.set(self.OPTIONAL, "prompt", input) 
-
     def addToPrompt(self, input, newLine=False):
         if newLine:
             self.requestDict[self.OPTIONAL]["prompt"] += "\n"
         self.requestDict[self.OPTIONAL]["prompt"] += input
-
-    def getMaxTokens(self):
-        return self.get(self.OPTIONAL, "max_tokens")
-
-    def setMaxTokens(self, input):
-        self.set(self.OPTIONAL, "max_tokens", input) 
-
-    def getTemperature(self):
-        return self.get(self.OPTIONAL, "temperature")
-
-    def setTemperature(self, input):
-        self.set(self.OPTIONAL, "temperature", input) 
-
-    def getTopP(self):
-        return self.get(self.OPTIONAL, "top_p")
-
-    def setTopP(self, input):
-        self.set(self.OPTIONAL, "top_p", input) 
-
-    def getN(self):
-        return self.get(self.OPTIONAL, "n")
-
-    def setN(self, input):
-        self.set(self.OPTIONAL, "n", input) 
-
-    def getStream(self):
-        return self.get(self.OPTIONAL, "stream")
-
-    def setStream(self, input):
-        self.set(self.OPTIONAL, "stream", input) 
-
-    def getEcho(self):
-        return self.get(self.OPTIONAL, "echo")
-
-    def setEcho(self, input):
-        self.set(self.OPTIONAL, "echo", input) 
-
-    def getPresencePenalty(self):
-        return self.get(self.OPTIONAL, "presence_penalty")
-
-    def setPresencePenalty(self, input):
-        self.set(self.OPTIONAL, "presence_penalty", input) 
-
-    def getFrequencyPenalty(self):
-        return self.get(self.OPTIONAL, "frequency_penalty")
-
-    def setFrequencyPenalty(self, input):
-        self.set(self.OPTIONAL, "frequency_penalty", input) 
-
-    def getBestOf(self):
-        return self.get(self.OPTIONAL, "best_of")
-
-    def setBestOf(self, input):
-        self.set(self.OPTIONAL, "best_of", input) 
-
-    def getUser(self):
-        return self.get(self.OPTIONAL, "user")
-
-    def setUser(self, input):
-        self.set(self.OPTIONAL, "user", input) 
 
 
 if __name__ == "__main__":
