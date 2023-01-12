b9219a1aa80ef3a80c0de3e1e012f8da9ab84293
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-11 20:11:54 -0600

    Lint baseframe.py
    Remove unnecessary spaces.

--- app/view/frame/baseframe.py
+++ app/view/frame/baseframe.py
@@ -5,7 +5,8 @@ import tkinter as tk
 class BaseFrame(tk.Frame):
     def __init__(self, window, controller):
         super().__init__(master=window)
-        self.controller=controller
+        self.controller = controller
+
 
 if __name__ == "__main__":
     pass
