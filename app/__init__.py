4522e7fdab704a869e135a10b83f41185917ca7d
Austin <austin.swanlaw@gmail.com>
2023-01-12 08:19:10 -0600

    Move files in project
    Move app.py out of package directory and rename package directory to
    openai-client.
    
    This should make working with the packages a bit easier. Eventually I
    would like to move to namespace packages (would make extending the
    project easy) but for now sticking to regular packages until I can get
    this working.

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-# app package __init__
-
-# import all modules from package
-from . import controller
-from . import model
-from . import view
-
-# packages to import
-__all__ = ["controller", "model", "view"]
