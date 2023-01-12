092ebbcddc8bb3e5360078e5aa581791aabfe1cb
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-01-11 20:21:47 -0600

    Add modules to __init__.py for frame module
    Added explicit calls to the packages in the frame package.
    
    This just makes it easier for my linter and language server to pick up
    what is going on and not throw a bunch of warnings and errors.

--- app/view/frame/__init__.py
+++ app/view/frame/__init__.py
@@ -1,7 +1,9 @@
 # frame package __init__
 
 # import all modules from package
-from . import *
+from . import baseframe
+from . import completionsettings
+from . import settingsframe
 
 # packages to import
-__all__ = []
+__all__ = ["baseframe", "completionsettings", "settingsframe"]
