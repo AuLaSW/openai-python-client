dbe31ea6dcd9bdedb82421d6acf809e88a2b570e
Austin <austin.swanlaw@gmail.com>
2023-01-13 10:41:48 -0600

    Create TestRequest class
    Create empty TestRequest class.
    
    This class tests the Request class.

---
+++ tests/unit/test_request.py
@@ -0,0 +1,12 @@
+# test_request.py
+from openaiclient.model.request.request import Request
+import unittest
+
+
+class TestRequest(unittest.Test):
+    def setup():
+        pass
+
+
+if __name__ == "__main__":
+    unittest.mainloop()
