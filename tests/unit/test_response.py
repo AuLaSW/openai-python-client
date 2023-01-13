4aa8d5f10d5f0c8952319a7daa87742f5935b816
Austin <austin.swanlaw@gmail.com>
2023-01-13 08:35:17 -0600

    Add unit test to test_response.py
    Add unit test to test that all object in a Response object are set
    correctly when it is initialized.

--- tests/unit/test_response.py
+++ tests/unit/test_response.py
@@ -4,17 +4,36 @@ import unittest
 
 
 class TestResponse(unittest.TestCase):
+    # test that the response object put
+    # the data in the correct objects
+    def test_correctPlacement(self):
+        self.assertEqual(self.obj, response["object"])
+        self.assertEqual(self.text, response["choices"][0]["text"])
+        self.assertEqual(self.index, response["choices"][0]["index"])
+        
+        if "text_completion" in self.obj:
+            self.assertEqual(self.model, response["model"])
+            self.assertEqual(
+                self.finish_reason, 
+                response["choices"][0]["finish_reason"]
+            )
+
+    # test that getText() returns the 
+    # text object
     def test_getText(self):
         result = self.response.getText()
         reference = self.dictionary["choices"][0]["text"]
 
         self.assertEqual(result, reference)
 
+    # test that getFinishReason() returns
+    # the finish reason
     def test_getFinishReason(self):
         result = self.response.getFinishReason()
         reference = self.dictionary["choices"][0]["finish_reason"]
 
         self.assertEqual(result, reference)
+
 
 
 class TestResponseTextCompletion(TestResponse):
