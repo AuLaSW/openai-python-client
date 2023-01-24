97085a980846640302a83d62147d87bac89368e7
Austin <austin.swanlaw@gmail.com>
2023-01-24 15:28:04 -0600

    Rewrite SettingsInputFrame class
    Rewrite the class to create the frame upon initialization of the object,
    to accept more inputs for kwargs, and to add the widgets to the frame.
    
    This greatly simplifies the use of the SettingsInputFrame class. The
    frame is entirely managed by the class and the object can then be
    attached to its parent frame by the parent frame. Just pass the
    functions and variables and then attach the frame to the settings
    window.

--- openaiclient/view/frame/settingsinputframe.py
+++ openaiclient/view/frame/settingsinputframe.py
@@ -3,42 +3,62 @@ from openaiclient.view.frame.baseframe import BaseFrame
 
 
 class SettingsInputFrame(BaseFrame):
-    def __init__(self, main, controller, label, default):
+    def __init__(self, main, controller, label, default, tkVar, tkFunc, **kwargs=dict()):
         super().__init__(self, main, controller)
 
         self.main = main
         self.label = label
         self.default = default
+        
+        # create the setting widgets
+        # and attach to the frame
+        self.addSettingWidget(
+            tkFunc,
+            kwargs | self.createOutput(tkVar)
+        )
 
     # default setting generator. Cleans
     # up the code and makes it easier to
     # define a new setting type
-    def addSettingWidget(self, tkFunc, label, **kwargs):
+    def addSettingWidget(self, tkFunc, **kwargs):
         # generate label on the left
         labelWidget = tk.Label(
             master=self,
-            text=label
+            text=self.label
+        ).grid(
+            column=0,
+            row=0,
+            padx=10,
+            pady=10
         )
 
         # setup the widget that we want
         # must pass the widget function
-        # through the function
+        # through the function and pass
+        # the kwargs with at least the
+        # option tracking variable to
+        # track what the entry value
+        # is
         widget = tkFunc(
             master=self,
             **kwargs
+        ).grid(
+            column=0,
+            row=0,
+            padx=10,
+            pady=10
         )
 
-        return labelWidget, widget
 
     # create the output variable for the widget
     # and point kwargs and self.main.options to it
-    def createOutput(self, tkVar, label, default):
+    def createOutput(self, tkVar):
         kwargs = dict()
 
         output = tkVar
-        output.set(default)
+        output.set(self.default)
 
-        self.main.options[label] = output
+        self.main.options[self.label] = output
 
         kwargs["variable"] = output
 
