b56c43e16a0bf7d783048691e89242391bb37323
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-02-04 22:42:54 -0600

    Refactor settingsframe.py
    A large refactoring. Much of the file was touched.
    
    Simplify call to SettingsInputFrame and update str/int/boolSetting()
    call to properly use SettingsInputFrame objects and methods for creating
    setting frames and attaching them to the main frame.

--- openaiclient/view/frame/settingsframe.py
+++ openaiclient/view/frame/settingsframe.py
@@ -3,23 +3,25 @@ import tkinter as tk
 from openaiclient.view.frame.baseframe import BaseFrame
 from openaiclient.view.frame.settingsinputframe import SettingsInputFrame
 
-"""
-Class SettingsFrame:
-
-This class is for a frame that holds settings for
-a user to change. Created to work with the Completion
-and Edit requests to OpenAI.
-"""
 
 
 class SettingsFrame(BaseFrame):
+    """
+    Class SettingsFrame:
+
+    This class is for a frame that holds settings for
+    a user to change. Created to work with the Completion
+    and Edit requests to OpenAI.
+    """
     def __init__(self, main, controller):
         super().__init__(main, controller)
 
+        # hold the settings we will be using
         self.settings = {}
-        self.options = {}
+        # hold the tk variables for returning inputs from the frame
         self.outputs = {}
 
+        # row count
         self.row = 0
 
     # creates all of the widgets for
@@ -28,37 +30,32 @@ class SettingsFrame(BaseFrame):
     # types of input in other settings
     # classes.
     def create(self):
+        """
+        Creates the settings input frames by looping through the settings
+        dictionary (self.settings) and creating a SettingsInputFrame object
+        for each setting. It then attaches each frame in the next row on
+        the frame with the grid() method.
+
+        Finally, it creates the save and exit buttons and attaches them to
+        the settings frame (self).
+        """
         for key, value in self.settings, self.settings.items():
             typeOfValue = type(value).__name__
+
+            inputFrame = SettingsInputFrame(
+                    self,
+                    self.controller,
+                    key,
+                    value
+                    )
+
             match typeOfValue:
                 case "str":
-                    # this won't work because the function
-                    # returns two values, so how to make
-                    # this work?
-                    labelWidget, widget = SettingsInputFrame(
-                        self,
-                        self.controller,
-                        key,
-                        value
-                    )
-
-                    labelWidget.grid(
-                        column=0,
-                        row=self.row,
-                        padx=10,
-                        pady=10,
-                    )
-
-                    widget.grid(
-                        column=0,
-                        row=self.row,
-                        padx=10,
-                        pady=10,
-                    )
+                    self.strSetting(inputFrame)
                 case "int":
-                    self.intSetting(key, value)
+                    self.intSetting(inputFrame)
                 case "bool":
-                    self.boolSetting(key, value)
+                    self.boolSetting(inputFrame)
                 case _:
                     pass
 
@@ -80,7 +77,7 @@ class SettingsFrame(BaseFrame):
                 return NotImplementedError()
             """
 
-        # save settings button at bottom of window
+        # button for savings settings
         tk.Button(
             master=self,
             label="Save",
@@ -92,7 +89,7 @@ class SettingsFrame(BaseFrame):
             pady=10
         )
 
-        # don't save settings button at bottom of window
+        # button to close the window
         tk.Button(
             master=self,
             label="Exit",
@@ -110,12 +107,28 @@ class SettingsFrame(BaseFrame):
     # default setting generator. Cleans
     # up the code and makes it easier to
     # define a new setting type
-    def baseSetting(self, tkFunc, label, **kwargs):
+    def baseSetting(self, tkVar, tkFunc, frame, **kwargs):
+        """
+        The function operates as follows:
+
+        1. This function appends the output variable for the setting input
+           frame to the kwargs dictionary. The output variable is generated
+           by passing tkVar to frame.createOutput().
+        2. Then, the function passes tkFunc and kwargs as arguments to
+           frame.addSettingWidget(), which returns the labelWidget and the
+           widget for inputting data.
+        3. The widgets are then placed onto the settings frame with the grid()
+           function on the current row.
+        """
+        kwargs = kwargs | frame.createOutput(tkVar)
+
+        labelWidget, widget = frame.addSettingWidget(
+            tkFunc,
+            **kwargs
+        )
+
         # generate label on the left
-        tk.Label(
-            master=self,
-            text=label
-        ).grid(
+        labelWidget.grid(
             column=0,
             row=self.row,
             padx=5,
@@ -125,10 +138,7 @@ class SettingsFrame(BaseFrame):
         # setup the widget that we want
         # must pass the widget function
         # through the function
-        tkFunc(
-            master=self,
-            **kwargs
-        ).grid(
+        widget.grid(
             column=1,
             row=self.row,
             padx=5,
@@ -139,39 +149,30 @@ class SettingsFrame(BaseFrame):
         self.row += 1
 
     # string setting input
-    def strSetting(self, label, default):
-        kwargs = dict()
-
-        self.addOutput(tk.StringVar, label, default, kwargs)
-
-        self.baseSetting(tk.Entry, label, **kwargs)
+    def strSetting(self, frame):
+        """
+        Creates a string setting input with the Entry object.
+        """
+        self.baseSetting(tk.StringVar, tk.Entry, frame)
 
     # integer setting input
-    def intSetting(self, label, default):
-        kwargs = dict()
-
-        self.addOutput(tk.IntVar, label, default, kwargs)
-
-        self.baseSetting(tk.Entry, label, **kwargs)
+    def intSetting(self, frame):
+        """
+        Creates an integer setting input with the Entry object.
+        """
+        self.baseSetting(tk.IntVar, tk.Entry, frame)
 
     # boolean setting input
-    def boolSetting(self, label, default):
-        kwargs = dict()
-
-        self.addOutput(tk.IntVar, label, default, kwargs)
-
+    def boolSetting(self, frame):
+        """
+        Creates a boolean setting input with on and off values as 1 and 0 and
+        the input as a checkbutton.
+        """
+        kwargs = {}
         kwargs["onvalue"] = 1
         kwargs["offvalue"] = 0
 
-        self.baseSetting(tk.Checkbutton, label, **kwargs)
-
-    def addOutput(self, tkVar, label, default, kwaDict):
-        output = tkVar
-        output.set(default)
-
-        self.outputs[label] = output
-
-        kwaDict["variable"] = output
+        self.baseSetting(tk.IntVar, tk.Checkbutton, frame, **kwargs)
 
 
 if __name__ == "__main__":
