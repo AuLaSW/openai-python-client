78b36a71e8f189f5e3fccbfd0ee95787d5e0028f
Austin <austin.swanlaw@gmail.com>
2023-02-14 13:22:52 -0600

    Remove generateModels() from Models class
    This function was getting to be too big and complicated. OpenAI
    describes the different models and what they can be used for, so instead
    the function was refactored to be a hard-coded list of available models.

--- openaiclient/model/models.py
+++ openaiclient/model/models.py
@@ -12,14 +12,19 @@ class Models:
     def __init__(self):
         self.COMPLETION = "Completion"
         self.EDIT = "Edit"
+        
         # dictionary of models
         self.models = {
-            self.COMPLETION: [],
-            self.EDIT: []
+            self.COMPLETION: [
+                "text-davinci-003",
+                "text-curie-001",
+                "text-babbage-001",
+                "text-ada-001"
+            ],
+            self.EDIT: [
+                "text-davinci-edit-001"
+            ]
         }
-
-        # populate the model dictionary
-        self.generateModels()
 
     def __str__(self):
         strModels = "Usable Models:\n"
@@ -38,38 +43,6 @@ class Models:
 
     def __contains__(self, item):
         return item in self.models
-
-    # returns a dictionary of usable text models
-    def generateModels(self):
-        # loop through the list of models
-        for model in openai.Model.list()["data"]:
-            # only focus on the id of the model
-            modelID = model["id"]
-
-            # is the model a text model
-            textModel = "text" in modelID
-            
-            # is the model an edit model?
-            editModel = "edit" in modelID
-
-            # is the model not a search or code model?
-            # I'm not sure what the : is used for in the model
-            # names, but I don't want to use those
-            codeModel = "code" in modelID
-            searchModel = "search" in modelID
-            simModel = "similarity" in modelID
-            insModel = "insert" in modelID
-            embModel = "embedding" in modelID
-            colModel = ":" in modelID
-            validModel = (not codeModel and not searchModel
-                          and not simModel and not colModel
-                          and not insModel and not embModel)
-
-            if textModel and validModel:
-                if editModel:
-                    self.models[self.EDIT].append(modelID)
-                else:
-                    self.models[self.COMPLETION].append(modelID)
 
     def getModels(self):
         return self.models
