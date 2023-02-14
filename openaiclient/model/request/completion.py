a8f46d7623f6e99db7352ed77620d50ad23d105a
Austin <austin.swanlaw@gmail.com>
2023-02-14 16:53:12 -0600

    Begin creating properties for completion.py

--- openaiclient/model/request/completion.py
+++ openaiclient/model/request/completion.py
@@ -70,6 +70,189 @@ class CompletionRequest(Request):
         else:
             self.requestDict["prompt"] += " " + newInput
 
+    # Properties
+    
+    ## Arguments
+    
+    @property
+    def requiredArguments(self):
+        return self.requiredArgs
+    
+    @property
+    def optionalArguments(self):
+        return self.optionalArgs
+    
+    @property
+    def settings(self):
+        return self.settings
+        
+    @settings.setter
+    def settings(self, vals):
+        try:
+            var, val = vals
+        except ValueError:
+            raise ValueError("Please pass an iterable with two values.")
+        
+        if var in self.settings:
+            self.requestDict[var] = val
+        else:
+            raise RuntimeError
+    
+    # request dictionary arguments
+    
+    @property
+    def model(self):
+        return self.requestDict["model"]
+    
+    """
+    TODO: link openaiclient.model.models to validate against known models.
+    """
+    @model.setter
+    def model(self, val):
+        if val in self.models:
+            self.requestDict["model"] = val
+        else:
+            raise RuntimeError(f"The model '{val}' is not a valid model.")
+
+    
+    @property
+    def prompt(self):
+        return self.requestDict["prompt"]
+        
+    @prompt.setter
+    def prompt(self, val):
+        if isinstance(val, string):
+            self.requestDict["prompt"] = val
+        else:
+            raise RuntimeError("The prompt must be a string.")
+    
+    @property
+    def max_tokens(self):
+        return self.requestDict["max_tokens"]
+        
+    """
+    TODO: validate maximum length
+    """
+    @max_tokens.setter
+    def max_tokens(self, val):
+        if isinstance(val, int) and val > 0:
+            self.requestDict["max_tokens"] = val
+        elif:
+            raise RuntimeError("max_tokens must be an integer greater than 0.")
+    
+    @property
+    def temperature(self):
+        return self.requestDict["temperature"]
+        
+    """
+    TODO: validate maximum length
+    """
+    @temperature.setter
+    def temperature(self, val):
+        if isinstance(val, int) and val > 0:
+            self.requestDict["temperature"] = val
+        elif:
+            raise RuntimeError("temperature must be an integer greater than 0.")
+        
+    @property
+    def top_p(self):
+        return self.requestDict["top_p"]
+        
+    """
+    TODO: validate maximum length
+    """
+    @top_p.setter
+    def top_p(self, val):
+        if isinstance(val, int) and val > 0:
+            self.requestDict["top_p"] = val
+        elif:
+            raise RuntimeError("top_p must be an integer greater than 0.")
+        
+    @property
+    def n(self):
+        return self.requestDict["n"]
+    
+    """
+    TODO: validate maximum length
+    """
+    @n.setter
+    def n(self, val):
+        if isinstance(val, int) and val > 0:
+            self.requestDict["top_p"] = val
+        elif:
+            raise RuntimeError("top_p must be an integer greater than 0.")
+        
+    @property
+    def stream(self):
+        return self.requestDict["stream"]
+        
+    @mstream.setter
+    def stream(self, val):
+        if isinstance(val, bool):
+            self.requestDict["stream"] = val
+        else:
+            raise RuntimeError("stream must be a boolean value.")
+        
+    @property
+    def echo(self):
+        return self.requestDict["echo"]
+        
+    @echo.setter
+    def echo(self, val):
+        if isinstance(val, bool):
+            self.requestDict["echo"] = val
+        else:
+            raise RuntimeError("echo must be a boolean value.")
+        
+    @property
+    def presence_penalty(self):
+        return self.requestDict["presence_penalty"]
+       
+    """
+    TODO: validate integer constraints
+    """
+    @presence_penalty.setter
+    def presence_penalty(self, val):
+        if isinstance(val, int) and val > 0:
+            self.requestDict["presence_penalty"] = val
+        elif:
+            raise RuntimeError("presence_penalty must be an integer greater than 0.")
+        
+    @property
+    def frequency_penalty(self):
+        return self.requestDict["frequency_penalty"]
+        
+    """
+    TODO: validate integer constraints
+    """
+    @frequency_penalty.setter
+    def frequency_penalty(self, val):
+        if isinstance(val, int) and val > 0:
+            self.requestDict["frequency_penalty"] = val
+        elif:
+            raise RuntimeError("frequency_penalty must be an integer greater than 0.")
+        
+    @property
+    def best_of(self):
+        return self.requestDict["best_of"]
+        
+    @best_of.setter
+    def best_of(self, val):
+        if isinstance(val, int) and val > 0:
+            self.requestDict["best_of"] = val
+        elif:
+            raise RuntimeError("best_of must be an integer greater than 0.")
+        
+    @property
+    def user(self):
+        return self.requestDict["user"]
+        
+    @user.setter
+    def user(self, val):
+        if isinstance(val, string):
+            self.requestDict["user"] = val
+        else:
+            raise RuntimeError("The user must be a string.")
 
 if __name__ == "__main__":
     pass
