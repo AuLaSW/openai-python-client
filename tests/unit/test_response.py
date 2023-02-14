be2eab64ce7e508789e186147c810b3ae5735205
Austin <austin.swanlaw@gmail.com>
2023-02-14 10:16:21 -0600

    Move TestResponse derived class into TestResponse
    Move the TestResponse(unittest.TestCase) class in test_response.py into
    the class TestResponse; create TestResponse class.
    
    By moving the class inside a class that is not derived from
    unittest.TestCase, the class will not run unless inherrited. This fixes
    the errors that the class was throwing since it is not designed to run
    on its own but to be a base class for more specific reponse test cases.

--- tests/unit/test_response.py
+++ tests/unit/test_response.py
@@ -3,31 +3,32 @@ import unittest
 from openaiclient.model.response import Response
 
 
-class TestResponse(unittest.TestCase):
-    def test_CorrectObject(self):
-        self.assertEqual(self.obj, response["object"])
+class TestResponse:
+    class TestResponse(unittest.TestCase):
+        def test_CorrectObject(self):
+            self.assertEqual(self.obj, response["object"])
 
-    def test_CorrectText(self):
-        self.assertEqual(self.text, response["choices"][0]["text"])
+        def test_CorrectText(self):
+            self.assertEqual(self.text, response["choices"][0]["text"])
 
-    def test_CorrectIndex(self):
-        self.assertEqual(self.index, response["choices"][0]["index"])
+        def test_CorrectIndex(self):
+            self.assertEqual(self.index, response["choices"][0]["index"])
 
-    # test that getText() returns the
-    # text object
-    def test_GetText(self):
-        result = self.response.getText()
-        reference = self.dictionary["choices"][0]["text"]
+        # test that getText() returns the
+        # text object
+        def test_GetText(self):
+            result = self.response.getText()
+            reference = self.dictionary["choices"][0]["text"]
 
-        self.assertEqual(result, reference)
+            self.assertEqual(result, reference)
 
-    # test that getFinishReason() returns
-    # the finish reason
-    def test_GetFinishReason(self):
-        result = self.response.getFinishReason()
-        reference = self.dictionary["choices"][0]["finish_reason"]
+        # test that getFinishReason() returns
+        # the finish reason
+        def test_GetFinishReason(self):
+            result = self.response.getFinishReason()
+            reference = self.dictionary["choices"][0]["finish_reason"]
 
-        self.assertEqual(result, reference)
+            self.assertEqual(result, reference)
 
 
 class TestResponseTextCompletion(TestResponse):
