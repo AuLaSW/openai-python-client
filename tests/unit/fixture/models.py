1d1fda8fa3a11af5522c77454f790dedb6ce52cb
Austin <austin.swanlaw@gmail.com>
2023-02-15 09:15:23 -0600

    Create fixture.models.py for testing
    Create a fixture models module for testing against without having to
    directly integrate the models module from the project.

---
+++ tests/unit/fixture/models.py
@@ -0,0 +1,20 @@
+"""
+This module replicates the Models class and contains the following:
+"""
+
+class Models:
+    def __init__(self):
+        self.models = {
+            "Completion": [
+                "text-davinci-003",
+                "text-curie-001",
+                "text-babbage-001",
+                "text-ada-001"
+            ],
+            "Edit": [
+                "text-davinci-edit-001"
+            ]
+        }
+    
+    def __contains__(self, item):
+        return item in self.models["Completion"] + self.models["Edit"]
