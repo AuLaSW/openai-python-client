24109ae4ac1d95c53889f759fef23957955ddfca
Austin <austin.swanlaw@gmail.com>
2023-02-15 11:05:07 -0600

    Add string failure test to test_models.py
    Add test_StrMethodUpdated() test to make sure that the __str__() method
    works with the current Models class objects.
    
    This doesn't test that the output is correct, just that the function can
    be run. Prevents it from not being updated if the rest of the class
    changes.

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -24,6 +24,18 @@ class TestModels(unittest.TestCase):
 
         self.assertNotIn(exModel, self.modelObj)
 
+    def test_StrMethodUpdated(self):
+        """
+        Tests that the __str__() method is updated to work with the class
+        """
+        try:
+            self.modelObj.__str__()
+        except Exception:
+            self.fail(
+                "The string function is not able to run!" + \
+                " Make sure you've updated the string function."
+            )
+
 
 if __name__ == "__main__":
     unittest.mainloop()
