5d850e9bb301dbf66fe7ec52959d1f8203d916ca
Austin <austin.swanlaw@gmail.com>
2023-02-15 10:48:07 -0600

    Remove test_ModelsFilterCorrectly()
    Remove the test from test_models.py
    
    The models are now hard-coded into the class instead of filtered from
    information from the API, so this test is no longer needed.

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -26,22 +26,6 @@ class TestModels(unittest.TestCase):
         # assert the completion models are not empty
         self.assertTrue(self.models[self.edit])
 
-    def test_ModelsFilterCorrectly(self):
-        # make sure that all models contain
-        # the word "text", since that means
-        # it is a text model
-        
-        for _, list in self.models.items():
-            for model in list:
-                with self.subTest(model=model):
-                    self.assertIn("text", model)
-                    self.assertNotIn("code", model)
-                    self.assertNotIn("search", model)
-                    self.assertNotIn("similarity", model)
-                    self.assertNotIn("insert", model)
-                    self.assertNotIn("embedding", model)
-                    self.assertNotIn(":", model)
-
     # tests that the model dictionary does
     # contain the model
     def test_ContainsModel(self):
