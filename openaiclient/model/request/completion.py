79c0cfd0e1125bb481ff525d49790b6862480b55
Austin <austin.swanlaw@gmail.com>
2023-02-17 13:47:14 -0600

    Rename requestDict to _requestDict in completion.py
    This is a hidden variable, so it has been renamed as such.
    
    One test failed, fixing it now.

--- openaiclient/model/request/completion.py
+++ openaiclient/model/request/completion.py
@@ -19,9 +19,9 @@ class CompletionRequest(Request):
         self._module = module
         self._models = models
 
-        # setup self.requestDict
+        # setup self._requestDict
 
-        self.requestDict = {
+        self._requestDict = {
             "model": "text-davinci-003",
             "prompt": "",
             "max_tokens": 16,
@@ -49,17 +49,17 @@ class CompletionRequest(Request):
         }
 
         # optional values
-        self.optionalArgs = set(self.requestDict.keys())
+        self.optionalArgs = set(self._requestDict.keys())
         self.optionalArgs -= self.requiredArgs
 
         # set of setting arguments
-        self._settings = set(self.requestDict.keys())
+        self._settings = set(self._requestDict.keys())
         self._settings.remove("prompt")
 
     def getResponse(self):
         return Response(
             self._module.Completion.create(
-                **self.requestDict,
+                **self._requestDict,
             )
         )
 
@@ -87,124 +87,124 @@ class CompletionRequest(Request):
     
     @property
     def model(self):
-        return self.requestDict["model"]
+        return self._requestDict["model"]
     
     def set_model(self, val):
         if val in self._models:
-            self.requestDict["model"] = val
+            self._requestDict["model"] = val
         else:
             raise RuntimeError(f"The model '{val}' is not a valid model.")
 
     
     @property
     def prompt(self):
-        return self.requestDict["prompt"]
+        return self._requestDict["prompt"]
         
     def set_prompt(self, val):
         if isinstance(val, str):
-            self.requestDict["prompt"] = val
+            self._requestDict["prompt"] = val
         else:
             raise RuntimeError("The prompt must be a string.")
     
     @property
     def max_tokens(self):
-        return self.requestDict["max_tokens"]
+        return self._requestDict["max_tokens"]
         
     def set_max_tokens(self, val):
         max_tokens = self._models.models[self.model].max_tokens
         
         if isinstance(val, int) and not isinstance(val, bool) and val > 0 and val <= max_tokens:
-            self.requestDict["max_tokens"] = val
+            self._requestDict["max_tokens"] = val
         else:
             raise RuntimeError(f"max_tokens must be an integer greater than 0 and less than {max_tokens}")
     
     @property
     def temperature(self):
-        return self.requestDict["temperature"]
+        return self._requestDict["temperature"]
 
     def set_temperature(self, val):
         if isinstance(val, float) and val >= 0 and val <=2:
-            self.requestDict["temperature"] = val
+            self._requestDict["temperature"] = val
         else:
             raise RuntimeError("temperature must be number between 0 and 2.")
         
     @property
     def top_p(self):
-        return self.requestDict["top_p"]
+        return self._requestDict["top_p"]
         
     def set_top_p(self, val):
         if isinstance(val, float) and val >= 0 and val <=1:
-            self.requestDict["top_p"] = val
+            self._requestDict["top_p"] = val
         else:
             raise RuntimeError("top_p must be an integer greater than 0.")
         
     @property
     def n(self):
-        return self.requestDict["n"]
+        return self._requestDict["n"]
     
     def set_n(self, val):
         if isinstance(val, int) and not isinstance(val, bool) and val > 0:
-            self.requestDict["n"] = val
+            self._requestDict["n"] = val
         else:
             raise RuntimeError("n must be an integer greater than 0.")
         
     @property
     def stream(self):
-        return self.requestDict["stream"]
+        return self._requestDict["stream"]
 
     def set_stream(self, val):
         if isinstance(val, bool):
-            self.requestDict["stream"] = val
+            self._requestDict["stream"] = val
         else:
             raise RuntimeError("stream must be a boolean value.")
         
     @property
     def echo(self):
-        return self.requestDict["echo"]
+        return self._requestDict["echo"]
 
     def set_echo(self, val):
         if isinstance(val, bool):
-            self.requestDict["echo"] = val
+            self._requestDict["echo"] = val
         else:
             raise RuntimeError("echo must be a boolean value.")
         
     @property
     def presence_penalty(self):
-        return self.requestDict["presence_penalty"]
+        return self._requestDict["presence_penalty"]
 
     def set_presence_penalty(self, val):
         if isinstance(val, float) and val >= -2.0 and val <= 2.0:
-            self.requestDict["presence_penalty"] = val
+            self._requestDict["presence_penalty"] = val
         else:
             raise RuntimeError("presence_penalty must be a float between -2.0 and 2.0.")
         
     @property
     def frequency_penalty(self):
-        return self.requestDict["frequency_penalty"]
+        return self._requestDict["frequency_penalty"]
         
     def set_frequency_penalty(self, val):
         if isinstance(val, float) and val >= -2.0 and val <= 2.0:
-            self.requestDict["frequency_penalty"] = val
+            self._requestDict["frequency_penalty"] = val
         else:
             raise RuntimeError("frequency_penalty must be a float between -2.0 and 2.0.")
         
     @property
     def best_of(self):
-        return self.requestDict["best_of"]
+        return self._requestDict["best_of"]
         
     def set_best_of(self, val):
         if isinstance(val, int) and not isinstance(val, bool) and val > 0:
-            self.requestDict["best_of"] = val
+            self._requestDict["best_of"] = val
         else:
             raise RuntimeError("best_of must be an integer greater than 0.")
         
     @property
     def user(self):
-        return self.requestDict["user"]
+        return self._requestDict["user"]
         
     def set_user(self, val):
         if isinstance(val, str):
-            self.requestDict["user"] = val
+            self._requestDict["user"] = val
         else:
             raise RuntimeError("The user must be a string.")
 
