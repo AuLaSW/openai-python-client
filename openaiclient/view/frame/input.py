9a328db206844efdef33ccd7288b8b0b6bf8d6fc
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-02-18 22:39:57 -0600

    Fix index error on insert() method
    Fix index error for text setter in Input() class.
    
    This uses index instead of just assigning val to char without calling
    char. This fixes errors that were being thrown.

--- openaiclient/view/frame/input.py
+++ openaiclient/view/frame/input.py
@@ -8,25 +8,27 @@ from openaiclient.view.frame.baseframe import BaseFrame
 class Input(BaseFrame):
     def __init__(self, main, controller):
         super().__init__(main, controller)
-        
+
         self._textBox = None
-    
+
     def create(self):
-        self._textBox = tk.Text(self).pack()
-    
+        self._textBox = tk.Text(self)
+        self._textBox.pack()
+
     @property
     def text(self):
         return self._textBox.get(
             index1=0,
             index2=tk.END
         )
-    
+
     @text.setter
     def text(self, val):
         self._textBox.insert(
             index=0,
-            val
+            chars=val
         )
+
 
 if __name__ == "__main__":
     window = tk.Tk()
