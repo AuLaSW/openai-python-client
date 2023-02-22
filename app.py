5d6a271d401f17542ab0e148b0cdc3f8a057e785
Austin <austin.swanlaw@gmail.com>
2023-02-22 15:59:01 -0600

    Add start() to app.py and remove openai import
    Add controller.start() to the app.py executable and remove the openai
    import temporarily.
    
    The openai import doesn't work for me at the moment (because I'm not on
    my home computer), so it'll have to wait until I get home to fully test.
    So far, though, it is going in the right direction.

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from os import environ
-import openai
+# import openai
+from tests.unit.fixture import api
+from openaiclient.controller.controller import Controller
 
 # get API key from usr folder
-openai.api_key = environ["OPENAI_API_KEY"]
+# openai.api_key = environ["OPENAI_API_KEY"]
+
+controller = Controller(api)
+
+controller.start()
