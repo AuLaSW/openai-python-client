e63d3387f98ac51ed9538ebefc15c8144016ce02
Austin <austin.swanlaw@gmail.com>
2023-02-23 09:31:49 -0600

    Update mainframe GUI
    Update the GUI to be a bit better-looking. Add buttons and descriptions
    for completion and edit requests.
    
    Still a lot more to do with this, but it's getting there. It's now a bit
    more organized.

--- openaiclient/view/frame/mainframe.py
+++ openaiclient/view/frame/mainframe.py
@@ -2,6 +2,9 @@
 mainframe module
 """
 import tkinter as tk
+from tkinter import ttk
+import math
+from increment import Increment
 from openaiclient.view.frame.baseframe import BaseFrame
 
 
@@ -15,32 +18,119 @@ class MainFrame(BaseFrame):
         super().__init__(window, controller)
 
     def create(self) -> BaseFrame:
+        col = Increment()
+        row = Increment()
+        
         self.createText().grid(
-            column=0,
-            row=0,
+            column=col,
+            row=row,
             columnspan=2
         )
+        
+        +row
+        
+        ttk.Separator(
+            self,
+            orient="horizontal",
+        ).grid(
+            column=col,
+            row=row,
+            columnspan=2,
+            sticky=tk.W+tk.E,
+            padx=10,
+            pady=5
+        )
+        
+        +row
+        
+        comp = tk.Text(
+            self,
+            width=30,
+            background=self.master['background'],
+            padx=0,
+            pady=0,
+            relief=tk.FLAT,
+            wrap=tk.WORD,
+        )
+        
+        comp.grid(
+            column=col,
+            row=row,
+            padx=15,
+            pady=5,
+            sticky=tk.W
+        )
+        
+        compLine = "This gives you access to the completion endpoint"
+        
+        comp.insert(
+            1.0,
+            compLine,
+        )
+        
+        self.master.update()
+        
+        comp['height'] = comp.count("1.0", "end", "displaylines")[0]
+        
+        +col
 
         tk.Button(
             self,
             text="Completion Endpoint",
-            command=self.controller.view.completionInputWindow
+            command=self.controller.view.completionInputWindow,
+            width=20,
+            height=2
         ).grid(
-            column=0,
-            row=1,
-            padx=10,
-            pady=10
+            column=col,
+            row=row,
+            padx=15,
+            pady=5   
         )
+
+        ~col
+        +row
+        
+        edit = tk.Text(
+            self,
+            width=30,
+            background=self.master['background'],
+            padx=0,
+            pady=0,
+            relief=tk.FLAT,
+            wrap=tk.WORD,
+        )
+        
+        edit.grid(
+            column=col,
+            row=row,
+            padx=15,
+            pady=5,
+        )
+        
+        editLine = "This gives you access to the edit endpoint"
+        
+        edit.insert(
+            1.0,
+            editLine,
+        )
+        
+        self.master.update()
+        
+        edit['height'] = edit.count("1.0", "end", "displaylines")[0]
+        
+        +col
 
         tk.Button(
             self,
             text="Edit Endpoint",
-            state=tk.DISABLED
+            state=tk.DISABLED,
+            width=20,
+            height=2
         ).grid(
-            column=1,
-            row=1,
-            padx=10,
-            pady=10
+            column=col,
+            row=row,
+            padx=15,
+            pady=5
         )
 
         return self
@@ -56,7 +146,7 @@ class MainFrame(BaseFrame):
             wrap=tk.WORD,
         )
 
-        line = "Hello! And welcome to openaiclient!\n\nPlease select an option below:"
+        line = "Hello! And welcome to openaiclient!"
 
         text.insert(
             1.0,
