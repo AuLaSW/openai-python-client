b9ee1d53b5ce6cf3e7c461a2d0e772237cd7d94a
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-02-25 21:01:45 -0600

    Change request.py to hold the request product classes
    This is part of the rewrite of the request and response package. This
    module will now hold the factories to create different request
    dictionaries.

--- openaiclient/model/request/request.py
+++ openaiclient/model/request/request.py
@@ -1,58 +1,212 @@
 """
-Class Request:
-
-An abstract class used to model different request types to the
-OpenAI API.
+Holds the request product classes
 """
 from abc import ABC, abstractmethod
 
 
-class Request(ABC):
-    def __init__(self):
-        # dictionary of request arguments
-        self.requestDict = dict()
-
-        # set of required request arguments
-        self.requiredArgs = {}
-
-        # set of optional request arguments
-        self.optionalArgs = {}
-
-        # set of request settings
-        self._settings = {}
-
+class RequestProduct(ABC):
     """
-    def __str__(self):
-        # this produces a header for the output that contains the
-        # class name (works with sublcasses, too)
-        strRequest = self.__class__.__name__ + " Input:\n"
-
-        # required dictionary and optional dictionary separation
-        requiredDict, optionalDict = self.separateDict()
-
-        # add required entries to the string
-        strRequest += "  Required Entries:\n"
-
-        for key in requiredDict:
-            strRequest += "    " + key + ": " + str(requiredDict[key]) + "\n"
-
-        # add optional entries to the string
-        strRequest += "  Optional Entries:\n"
-
-        for key in optionalDict:
-            strRequest += "    " + key + ": " + str(optionalDict[key]) + "\n"
-
-        return strRequest
+    Abstract Product class for Request Products
     """
 
-    # returns response object from request
+    def __getattr__(self, name):
+        if '_requestDict' in self.__dict__:
+            return self.__dict__['_requestDict'][name]
+        else:
+            pass
+
+    """
+    @property
     @abstractmethod
-    def getResponse(self):
-        raise NotImplementedError
+    def requiredArguments(self):
+        pass
 
     @property
+    @abstractmethod
+    def optionalArguments(self):
+        pass
+
+    @property
+    @abstractmethod
     def settings(self):
-        if len(self._settings) == 0:
-            raise RuntimeError("No settings to return")
-        else:
-            return self._settings
+        pass
+
+    @property
+    @abstractmethod
+    def request(self):
+        pass
+    """
+
+
+class CompletionRequest(RequestProduct):
+    """
+    A Completion Request object
+    """
+
+    def __init__(self, model=None) -> None:
+        # I'm wondering if this should just be read from a database of
+        # default values for differnt types of requests? Would be easier
+        # than trying to type it all out programatically.
+        self._requestDict = {
+            "model": RequestSetting(
+                value=model,
+                setting=True,
+                optional=False,
+                ),
+            "prompt": RequestSetting(
+                value="",
+                setting=False,
+                optional=True,
+                ),
+            "max_tokens": RequestSetting(
+                value=16,
+                setting=True,
+                optional=True,
+                ),
+            "temperature": RequestSetting(
+                value=0.0,
+                setting=True,
+                optional=True,
+                ),
+            "top_p": RequestSetting(
+                value=0.0,
+                setting=True,
+                optional=True,
+                ),
+            "n": RequestSetting(
+                value=1,
+                setting=True,
+                optional=True,
+                ),
+            "stream": RequestSetting(
+                value=False,
+                setting=True,
+                optional=True,
+                ),
+            "echo": RequestSetting(
+                value=False,
+                setting=True,
+                optional=True,
+                ),
+            "presence_penalty": RequestSetting(
+                value=0.0,
+                setting=True,
+                optional=True,
+                ),
+            "frequency_penalty": RequestSetting(
+                value=0.0,
+                setting=True,
+                optional=True,
+                ),
+            "best_of": RequestSetting(
+                value=1,
+                setting=True,
+                optional=True,
+                ),
+            "user": RequestSetting(
+                value="",
+                setting=True,
+                optional=True,
+                ),
+            # these keys I cannot get to work and are optional,
+            # so they are commented out until they work
+            #
+            # "suffix": NULL,
+            # "logprobs": 0,
+            # "stop": "",
+            # "logit_bias": {},
+        }
+
+    @property
+    def request(self):
+        """
+        Returns a cleaned dictionary of the request dictionary
+        """
+        temp = {}
+
+        for key, item in self._requestDict.items():
+            temp[key] = item.value
+
+        return temp
+
+    @property
+    def optionalArguments(self):
+        """
+        Returns a dictionary of optional arguments for the request
+        """
+
+        temp = {}
+
+        for key, item in self._requestDict.items():
+            if item.optional:
+                temp[key] = item.value
+
+        return temp
+
+    @property
+    def requiredArguments(self):
+        """
+        Returns a dictionary of optional arguments for the request
+        """
+
+        temp = {}
+
+        for key, item in self._requestDict.items():
+            if not item.optional:
+                temp[key] = item.value
+
+        return temp
+
+
+class RequestSetting:
+    """
+    Holds setting objects for request objecs
+    """
+
+    def __init__(self, value, setting=False, optional=False) -> None:
+        self._value = value
+        self._setting = setting
+        self._optional = optional
+
+    def set(self, name, val) -> None:
+        datafield = getattr(self, name, None)
+        if type(val) == type(datafield):
+            setattr(self, name, val)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        self.set('_value', val)
+
+    @property
+    def setting(self):
+        return self._setting
+
+    @setting.setter
+    def setting(self, val):
+        self.set('_setting', val)
+
+    @property
+    def optional(self):
+        return self._optional
+
+    @optional.setter
+    def optional(self, val):
+        self.set('_optional', val)
+
+
+if __name__ == "__main__":
+    setting = RequestSetting(1)
+
+    setting.setting = True
+
+    setting.optional = True
+
+    print(setting.value)
+
+    req = CompletionRequest()
+
+    print(req.optionalArguments)
