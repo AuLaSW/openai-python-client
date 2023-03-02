91e3091d2ea56d6a6adc0ed33cf3371822871846
Austin Swanlaw <austin.swanlaw@gmail.com>
2023-03-01 21:07:59 -0600

    Rename files

---
+++ openaiclient/model/modelsproduct.py
@@ -0,0 +1,83 @@
+from abc import ABC, abstractmethod
+import pickle
+from pathlib import Path
+
+
+class ModelProduct(ABC):
+    """
+    Abstract Product class for Model Products
+    """
+
+    def __getattr__(self, name):
+        if '_models' in self.__dict__:
+            return self.__dict__['_models'][name]
+
+    def _pickle_dump(self):
+        with open(self.PICKLE_PATH, "wb") as file:
+            pickle.dump(self._models, file)
+
+
+class CompletionModels(ModelProduct):
+    """
+    A completion model object
+    """
+
+    def __init__(self):
+        self.PICKLE_PATH = Path(
+            f"./defaults/model/request/{self.__class__.__name__}.pickle"
+        )
+        print(self.PICKLE_PATH)
+
+        self._models = {}
+
+        try:
+            with open(self.PICKLE_PATH, "rb") as file:
+                self._models = pickle.load(file)
+        except Exception:
+            self._models = {
+                "text_davinci_003": Model(
+                    name="text-davinci-003",
+                    max_tokens=4_000,
+                ),
+                "text_curie_001": Model(
+                    name="text-curie-001",
+                    max_tokens=2_048,
+                ),
+                "text_babbage_001": Model(
+                    name="text-babbage-001",
+                    max_tokens=2_048,
+                ),
+                "text_ada_001": Model(
+                    name="text-ada-001",
+                    max_tokens=2_048,
+                ),
+                # "text_davinci_edit_001": Model(
+                #    name="text-davinci-edit-001",
+                #    endpoint=self.EDIT
+                # ),
+            }
+
+    @property
+    def models(self):
+        return self._models
+
+
+class Model:
+    def __init__(self, name, max_tokens=-1):
+        self._name = name
+        self._max_tokens = max_tokens
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def max_tokens(self):
+        return self._max_tokens
+
+
+if __name__ == "__main__":
+    model = CompletionModels()
+    model._pickle_dump()
+
+    print(model)
--- openaiclient/model/responseproduct.py
+++ openaiclient/model/responseproduct.py
@@ -8,28 +8,6 @@ from abc import ABC, abstractmethod
 from tests.unit.fixture import api
 
 
-"""
-class Response:
-    def __init__(self, response):
-        self.obj = response["object"]
-        self.text = response["choices"][0]["text"]
-        self.index = response["choices"][0]["index"]
-
-        if "text_completion" in self.obj:
-            self.model = response["model"]
-            self.finish_reason = response["choices"][0]["finish_reason"]
-
-    def getText(self):
-        return self.text
-
-    def getFinishReason(self):
-        try:
-            return self.finish_reason
-        except AttributeError:
-            raise RuntimeError
-"""
-
-
 class ResponseProduct(ABC):
     """
     An interface for working with response products
